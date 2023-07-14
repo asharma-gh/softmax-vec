52c2ec49ca2a958360e6b5ac33049599578aa379
Arvin Sharma <sharma.arv@husky.neu.edu>
2023-07-14 00:49:12 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import numpy as np
+import imageio.v2  as imageio
+from tensorflow.keras import Sequential as Seq
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.datasets import mnist
+from sklearn.metrics import classification_report
+
+def _init_img_data():
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+  x_train = x_train / 255
+  x_test = x_test / 255
+
+  # Convert Y to one-hot encoding
+  y_train_t = np.zeros((y_train.shape[0], y_train.max() + 1))
+  y_train_t[np.arange(y_train.shape[0]), y_train] = 1
+
+  y_test_t = np.zeros((y_test.shape[0], y_test.max() + 1))
+  y_test_t[np.arange(y_test.shape[0]), y_test] = 1 ## y_test_t[row(0->N), y_test[0->N]] = 1
+
+  return x_test, x_train, y_train_t, y_test_t, y_train, y_test
+
+
+def flatten_bin_img(X):
+    # Flattens the input binary image from (n, l, w) to (n, l*w)
+    return X.reshape(X.shape[0], -1)
+
+# Custom Softmax implementation
+def custom_impl():
+  from softmax import Layer, LINEAR, \
+    initialize_parameters, \
+    L_model_f_prop, \
+    compute_crossentropy_loss, \
+    L_model_b_prop, \
+    update_params, \
+    predict
+  x_test, x_train, y_train, y_test, y_train_orig, y_test_orig = _init_img_data()
+  x_train = flatten_bin_img(x_train).T
+  x_test = flatten_bin_img(x_test).T
+
+  param = initialize_parameters([
+    Layer(
+      layer_name="Input",
+      layer_dims=x_test.shape[0],
+      activation_function=None
+    ),      
+    Layer(
+      layer_name="L1",
+      layer_dims=150
+    ),
+    Layer(
+      layer_name="Output",
+      layer_dims=10,
+      activation_function=LINEAR
+    )
+  ])
+  grads = None
+  for ii in range(1000):
+    R, ca = L_model_f_prop(x_test, param)
+    if ii % 100 == 0:
+      print(f"Cost: {compute_crossentropy_loss(R, y_test)}")
+    grads = L_model_b_prop(R, y_test, ca)
+    param = update_params(param, grads, .05)
+  res = predict(param, x_test)
+  print(classification_report(y_test_orig, res))
+
+
+## Tensorflow implementation for Softmax classification
+def tf_impl():
+  '''
+      accuracy  0.97     10000
+  '''
+  x_test, x_train, y_train, y_test = _init_img_data()
+
+  model = Seq([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu', name="L1"),
+    Dense(10, activation='linear', name="L2")
+  ])
+  model.compile(
+      loss=SparseCategoricalCrossentropy(from_logits=True)
+  )
+  model.fit(
+      x_train,
+      y_train,
+      epochs=3
+  )
+  res =  np.argmax(model.predict(x_test), axis=1)
+  print(classification_report(y_test, res))
+
+
+
+## Run
+custom_impl()
---
+++ softmax.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+RELU = 'RelU'
+SOFTMAX = 'Softmax'
+LINEAR = 'Linear'
+SIGMOID = 'Sigmoid'
+
+class Layer(object):
+    def __init__(self, layer_name, layer_dims=1, activation_function=RELU):
+        self.layer_name = layer_name
+        self.layer_dims = layer_dims
+        self.activation_function = activation_function
+# end class
+
+
+### Softmax Regression ###
+
+# Initialize parameters for NN for each Layer object in Ldims
+def initialize_parameters(Ldims):
+    parameters = {}
+    TL = len(Ldims)
+    for l in range(1, TL):
+        parameters[f'W{l}'] = np.random.randn(Ldims[l].layer_dims, Ldims[l - 1].layer_dims) * .01
+        parameters[f'b{l}'] = np.zeros((Ldims[l].layer_dims, 1))
+        parameters[f'layer_obj-{l}'] = Ldims[l]
+
+    return parameters
+
+
+def _dRelU(Ab, cache):
+    Z = cache
+    res = (1. * (Z >= 0)).reshape(Z.shape)
+    return Ab * res
+
+def _relU(Z):
+    return np.multiply(Z, Z > 0), Z
+
+def sigmoid(Z):
+    return 1/(1+ np.exp(-1*Z)), Z
+
+def _dSigmoid(Ab, cache):
+    Z = cache
+    ss, _ = sigmoid(Z)
+    return Ab * (ss * (1 - ss))
+
+def softmax(Z):
+    # Input tensor containing [z1....zn]
+    s = np.max(Z)
+    ez = np.exp(Z - s)
+    return ez / np.sum(ez, axis=0, keepdims=True) # broadcasts sum over each e^z
+
+## Forward propagation
+# F prop with basic linear regression        
+def _lin_f_prop(A, W, b):
+    Z = W.dot(A) + b
+    return Z, (A,W,b)
+
+# F prop with activation
+def lin_af(Aprev, W, b, act=RELU):
+    Z, lc = _lin_f_prop(Aprev, W, b)
+    if act == RELU:
+        A, ac = _relU(Z)
+    elif act == LINEAR:
+        A, ac = Z, Z      
+    else:
+        raise ValueError("Could not determine activation func: {}".format(act))
+    
+    return A, (lc, ac)
+
+def L_model_f_prop(X, parameters):
+    # perform X-> [LDims_i]->Yhat
+    Ldims = len(parameters) // 3 # W,b,Layer for each dimension
+    A = X
+    caches = []
+    # Apply forward prop on each layer 
+    for l in range(1, Ldims+1):
+        A, cache = lin_af(A, 
+                          parameters[f'W{l}'], 
+                          parameters[f'b{l}'], 
+                          parameters[f'layer_obj-{l}'].activation_function
+                        )
+        caches.append(cache)
+    return A, caches
+
+## Loss function
+def compute_crossentropy_loss(Z, Y):
+    eZ = softmax(Z)
+    # Y is one hot encoded (N x K), k = num categories
+    res = np.multiply(eZ, Y.T)
+    total = res.sum(axis=1).sum(axis=0) # sum each category together, then all categories for average
+
+    return -1 * total/Y.shape[0]
+
+def compute_theshold_softmax(Z):
+    eZ = softmax(Z)
+    return np.argmax(eZ, axis = 0)
+## Back propagation
+
+def _lin_b_prop(dZ, cache):
+    Aprev, W, b = cache
+    n = Aprev.shape[1]
+
+    dW = dZ.dot(Aprev.T) / n
+    db = np.sum(dZ, axis=1, keepdims=True) / n
+    dA_prev = W.T.dot(dZ)
+
+    return dA_prev, dW, db
+
+def lin_act_b_prop(dA, cache, act):
+    # bprop with activation d/dx
+    linear_cache, activation_cache = cache
+    dZ = None
+    if act == RELU:
+        dZ = _dRelU(dA, activation_cache)
+    elif act == SIGMOID:
+        dZ = _dSigmoid(dA, activation_cache)    
+        pass
+    elif act == LINEAR:
+        dZ = dA
+    else:
+        raise ValueError("Could not determine activation func: {}".format(act))
+    
+    dA_prev, dW, db = _lin_b_prop(dZ, linear_cache)
+    return dA_prev, dW, db
+
+def L_model_b_prop(AL, Y, caches):
+    Ldims = len(caches)
+
+    grads = {}
+    cur_cache = caches[Ldims - 1]
+    # dAL = dL/dA * dA/dz (softmax)
+    dAL = AL-Y.T # Y is 1-hot encoded for the relevent category
+    dA_prt, dW_t, db_t = lin_act_b_prop(dAL, cur_cache, "Linear")
+    grads["dA" + str(Ldims-1)] = dA_prt
+    grads["dW" + str(Ldims)] = dW_t 
+    grads["db" + str(Ldims)] = db_t
+
+    for l in reversed(range(Ldims-1)):
+        current_cache = caches[l]
+        dA_prt, dW_t, db_t = lin_act_b_prop(grads["dA" + str(l + 1)], current_cache, "Sigmoid")
+        grads["dA" + str(l)] = dA_prt
+        grads["dW" + str(l + 1)] = dW_t 
+        grads["db" + str(l + 1)] = db_t
+
+    return grads
+
+def update_params(params, grads, lr):
+    params = params.copy()
+    Ldims =  len(params) // 3
+    for l in range(Ldims):
+        params["W" + str(l+1)] = params["W" + str(l+1)] - lr*grads["dW" + str(l+1)]
+        params["b" + str(l+1)] = params["b" + str(l+1)] - lr*grads["db" + str(l+1)]
+    return params
+
+def predict(params, input):
+    Z, _ = L_model_f_prop(input, params)
+    return compute_theshold_softmax(Z)
